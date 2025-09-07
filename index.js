afcb67c89a81a1a773479d124e05bb7c0b6ebb97
Sunaram Patir <sunaram@gmail.com>
2025-09-07 22:41:12 +0530

    fix

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-export default errorHandler = (err, req, res, next) => {
+const errorHandler = (err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ message: err.message || "Something went wrong" });
 }
+
+export default errorHandler;
